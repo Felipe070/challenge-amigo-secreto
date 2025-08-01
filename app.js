10544ec5898e39c32a63b0ea23eb8080eb59eb89
Felipe070 <felipepravi09@gmail.com>
2025-08-01 12:00:11 -0300

    Declaro la función de agregarAmigo() y limpiarCaja()

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresIngresados = [];
+
+
+function limpiarCaja() {
+  document.getElementById("amigo").value = "";
+  return;
+}
+
+function agregarAmigo(){
+    let valorIngresado = document.getElementById("amigo").value;
+
+    if (valorIngresado == "") {
+        alert(`Debes ingresar un nombre`);
+    } else {
+        nombresIngresados.push(valorIngresado);
+    }
+
+    limpiarCaja();
+
+    return console.log(nombresIngresados);
+}
+
