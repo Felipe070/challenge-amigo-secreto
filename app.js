13a9bf07305937592337aea852d065a82207cf78
Felipe070 <felipepravi09@gmail.com>
2025-08-04 11:18:09 -0300

    Agrego funcion para enlistar los amigos ingresados

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-let nombresIngresados = [];
-
+let amigos = [];
 
 function limpiarCaja() {
   document.getElementById("amigo").value = "";
   return;
 }
 
-function agregarAmigo(){
-    let valorIngresado = document.getElementById("amigo").value;
+function agregarAmigo() {
+  let valorIngresado = document.getElementById("amigo").value;
 
-    if (valorIngresado == "") {
-        alert(`Debes ingresar un nombre`);
-    } else {
-        nombresIngresados.push(valorIngresado);
-    }
+  if (valorIngresado == "") {
+    alert(`Debes ingresar un nombre`);
+  } else {
+    amigos.push(valorIngresado);
+  }
 
-    limpiarCaja();
-
-    return console.log(nombresIngresados);
+  limpiarCaja();
+  recorrerAmigos(amigos);
+  console.log(amigos);
+  return amigos;
 }
 
+function recorrerAmigos(array) {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = array[i];
+    listaAmigos.appendChild(li);
+  }
+  return;
+}
